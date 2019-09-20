b1d296816a972ce84bd470225d10e4e1659e1734
VR <vr@VRs-MacBook-Air.local>
2019-09-20 15:56:56 +0300

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-const gulp = require("gulp");
-const del = require("del");
-const plumber = require("gulp-plumber");
-const browserSync = require("browser-sync");
+const gulp = require('gulp');
+const del = require('del');
+const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync');
 
 // Styles
-const sass = require("gulp-sass");
+const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-const prefix = require("gulp-autoprefixer");
-const sourcemaps = require("gulp-sourcemaps");
-const minify = require("gulp-cssnano");
+const prefix = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const minify = require('gulp-cssnano');
 const cleanCss = require('gulp-clean-css');
 
 // Scripts
@@ -17,45 +18,68 @@ const uglify = require('gulp-uglify');
 
 // HTML
 const fileinclude = require('gulp-file-include');
-// const htmlmin = require('gulp-htmlmin');
+const htmlmin = require('gulp-htmlmin');
+
+// Images
+const imagemin = require('gulp-imagemin');
+
+// ------------------------ TASKS ------------------------
 
 const PATHS = {
-  input: "src/",
-  output: "./dist/",
+  input: 'src/',
+  output: './dist/',
   scripts: {
-    input: "src/js/*.js",
+    input: 'src/js/*.js',
   },
   styles: {
-    input: "./src/sass/**/*.scss",
+    input: './src/sass/**/*.scss',
   },
   html: {
-    input: "src/html/pages/*.html",
-    watch: "./src/html/**/*.html"
+    input: 'src/html/pages/*.html',
+    watch: './src/html/**/*.html'
   },
   images: {
-    input: "src/images/**/*",
+    input: 'src/img/**/*',
   },
-  reload: "./dist/"
+  reload: './dist/'
 };
 
-
-gulp.task("clean", function () {
-  return del([ "dist" ]);
+gulp.task('clean', function () {
+  return del([ 'dist' ]);
 });
 
-gulp.task("fileinclude", function (cb) {
-  gulp.src(PATHS.html.input)
+gulp.task('move', function () {
+  return gulp.src([ './src/assets/**/*', './src/php/**/*.php' ])
+    .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('images', function () {
+  return gulp.src(PATHS.images.input)
+    .pipe(imagemin())
+    .pipe(gulp.dest(PATHS.output + '/img/'));
+});
+
+gulp.task('fileinclude', function () {
+  return gulp.src(PATHS.html.input)
     .pipe(plumber())
     .pipe(fileinclude({
       prefix: '#',
       basepath: './src/html/pages'
     }))
-    // .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./dist'));
-  cb();
 });
 
-gulp.task("css:dev", function () {
+gulp.task('fileinclude:prod', function () {
+  return gulp.src(PATHS.html.input)
+    .pipe(fileinclude({
+      prefix: '#',
+      basepath: './src/html/pages'
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css:dev', function () {
   return gulp
     .src(PATHS.styles.input)
     .pipe(sourcemaps.init())
@@ -66,14 +90,14 @@ gulp.task("css:dev", function () {
     .pipe(browserSync.stream());
 });
 
-gulp.task("css:prod", function () {
+gulp.task('css:prod', function () {
   return gulp
     .src(PATHS.styles.input)
     .pipe(sass())
     .pipe(cleanCss())
     .pipe(
       prefix({
-        browsers: [ "last 2 version", "> 0.25%" ],
+        browsers: [ 'last 2 version', '> 0.25%' ],
         cascade: true,
         remove: true
       })
@@ -88,41 +112,42 @@ gulp.task("css:prod", function () {
     .pipe(gulp.dest(PATHS.output));
 });
 
-gulp.task("js:dev", function () {
+gulp.task('js:dev', function () {
   return gulp
-    .src(PATHS.scripts.input)
+    .src([ 'src/js/libs/*.js', 'src/js/index.js' ])
+    .pipe(concat('index.js'))
     .pipe(gulp.dest(PATHS.output))
 })
 
-gulp.task("js:prod", function () {
-  return gulp
-    .src(PATHS.scripts.input)
+gulp.task('babel', function () {
+  return gulp.src('src/js/index.js')
     .pipe(babel({
       presets: [ '@babel/env' ]
     }))
     .pipe(uglify())
     .pipe(gulp.dest(PATHS.output))
-})
+});
 
-gulp.task("watch", function () {
+gulp.task('js:prod', function () {
+  return gulp
+    .src([ 'src/js/libs/*.js', 'dist/index.js' ])
+    .pipe(concat('index.js'))
+    .pipe(gulp.dest(PATHS.output))
+});
+
+gulp.task('watch', function () {
   browserSync.init({
     server: {
-      baseDir: "./dist/"
+      baseDir: './dist/'
     }
   });
 
-  gulp.watch("*.html").on("change", browserSync.reload);
-  gulp.watch(PATHS.styles.input).on("change", gulp.series([ "css:dev" ], browserSync.reload));
-  gulp.watch(PATHS.scripts.input).on("change", gulp.series([ "js:dev" ], browserSync.reload));
-  gulp.watch(PATHS.images.input).on("change", gulp.series([ "copyImages" ], browserSync.reload));
-  gulp.watch(PATHS.html.watch).on('change', gulp.series([ "fileinclude" ], browserSync.reload));
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch(PATHS.styles.input).on('change', gulp.series('css:dev'), browserSync.reload);
+  gulp.watch(PATHS.scripts.input).on('change', gulp.series('js:dev'), browserSync.reload);
+  gulp.watch(PATHS.images.input).on('change', gulp.series('images'), browserSync.reload);
+  gulp.watch(PATHS.html.watch).on('change', gulp.series('fileinclude'), browserSync.reload);
 });
 
-gulp.task("copyImages", function () {
-  return gulp.src(PATHS.images.input).pipe(gulp.dest(PATHS.output + '/images/'));
-});
-
-gulp.task("build", function (cb) {
-  gulp.series("css:prod");
-  cb();
-});
+gulp.task('default', gulp.series('clean', 'css:dev', 'js:dev', 'images', 'fileinclude', 'move', 'watch'));
+gulp.task('build', gulp.series('clean', 'fileinclude:prod', 'babel', 'css:prod', 'js:prod', 'images', 'move'));
--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,119 @@
-const BUTTON_FORM = document.querySelector('.button[type=submit]');
-const PHONE_INPUT = document.querySelector('.subscribe__input[name=phone]');
+document.addEventListener('DOMContentLoaded', () => {
+	// Form send
+	(function () {
+		const BUTTON_FORM = document.querySelector('.button[type=submit]');
+		const PHONE_INPUT = document.querySelector('.subscribe__input[name=phone]');
 
-// PHONE.addEventListener('input', () => {
-//   this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-// });
+		PHONE_INPUT.addEventListener('input', (event) => {
+			PHONE_INPUT.value = PHONE_INPUT.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+		});
 
-BUTTON_FORM.addEventListener('click', () => {
+		BUTTON_FORM.addEventListener('click', () => {
 
-	if (PHONE_INPUT && PHONE_INPUT.value.length > 0) {
-		sendPhone(PHONE_INPUT.value);
-		PHONE_INPUT.value = "";
-		toggleModal();
-	} else {
-		PHONE_INPUT.classList.toggle('shake');
+			if (PHONE_INPUT && PHONE_INPUT.value.length > 0) {
+				sendPhone(PHONE_INPUT.value);
+				PHONE_INPUT.value = "";
+			} else {
+				PHONE_INPUT.classList.toggle('shake');
 
-		setTimeout(() => {
-			PHONE_INPUT.classList.toggle('shake');
-		}, 500);
-	}
+				setTimeout(() => {
+					PHONE_INPUT.classList.toggle('shake');
+				}, 500);
+			}
+		});
+
+		const sendPhone = (data) => {
+			if (!data) return;
+
+			const url = 'post.php';
+			const request = new XMLHttpRequest();
+
+			request.open('POST', url, true);
+			request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+			request.send("phone=" + data);
+			request.onload = function () {
+				if (this.status === 200 && this.status < 400) {
+					toggleModal();
+				} else {
+					toggleModal(true);
+				}
+			}
+			request.onerror = function () {
+				toggleModal(true);
+			}
+		};
+
+		const toggleModal = (isError) => {
+			const BACKDROP = document.querySelector(".backdrop");
+			const MODAL = document.querySelector('.modal');
+			const MODAL_STATUS = MODAL.querySelector(".modal__icon-status");
+			const MODAL_STATUS_TITLE = MODAL.querySelector('.modal__title');
+			const MODAL_STATUS_SUBTITLE = MODAL.querySelector('.modal__subtitle');
+			const MODAL_BTN = MODAL.querySelector(".button");
+			const CLOSE_BTN = MODAL.querySelector(".modal__close-btn");
+
+			let modalInitTitle = "Спасибо за заявку!";
+			let modalInitSubtitle = "Мы вам перезвоним в течении <br><b>30 минут</b>.";
+
+			if (isError) {
+				const MODAL_STATUS = MODAL.querySelector('.modal__icon-status');
+				MODAL_STATUS.classList.add('modal__icon-status--error');
+				MODAL_STATUS_TITLE.innerText = "Упс, что-то пошло не так :(";
+				MODAL_STATUS_SUBTITLE.innerText = "";
+			} else {
+				MODAL_STATUS_TITLE.innerText = modalInitTitle;
+				MODAL_STATUS_SUBTITLE.innerHTML = modalInitSubtitle;
+				MODAL_STATUS.classList.remove('modal__icon-status--error');
+			}
+
+			BACKDROP.classList.toggle('backdrop--open');
+			MODAL.classList.toggle('modal--open');
+			document.body.classList.toggle('fixed');
+
+			const closeModal = () => {
+				BACKDROP.classList.remove('backdrop--open');
+				MODAL.classList.remove('modal--open');
+				document.body.classList.remove('fixed');
+			}
+
+			if (BACKDROP) BACKDROP.addEventListener('click', closeModal);
+			if (MODAL_BTN) MODAL_BTN.addEventListener('click', closeModal);
+			if (CLOSE_BTN) CLOSE_BTN.addEventListener('click', closeModal);
+		}
+	})();
+
+	// Images zoom
+	(function () {
+		const IMAGES = document.querySelectorAll('[data-zoom]');
+		if (IMAGES) {
+			mediumZoom('[data-zoom]', {
+				background: 'rgba(0, 0, 0, 0.8)',
+			});
+		}
+	})();
+
+	// Mobile menu
+	(function () {
+		const NAV_BURGER = document.querySelector('.nav__button');
+		const NAV_MOBILE = document.querySelector('.nav-mobile');
+		const NAV_MOBILE_CLOSE = NAV_MOBILE.querySelector('.nav-mobile__close');
+		const BACKDROP = document.querySelector(".backdrop");
+
+		const openMenu = () => {
+			BACKDROP.classList.add('backdrop--open');
+			NAV_MOBILE.classList.add('nav-mobile--open');
+		}
+		const closeMenu = () => {
+			BACKDROP.classList.remove('backdrop--open');
+			NAV_MOBILE.classList.remove('nav-mobile--open');
+		}
+
+		if (BACKDROP) BACKDROP.addEventListener('click', closeMenu);
+
+		NAV_BURGER.addEventListener('click', openMenu);
+
+		NAV_MOBILE_CLOSE.addEventListener('click', closeMenu);
+
+	})();
 });
 
-const sendPhone = (data) => {
-	if (!data) return;
-
-	const url = 'post.php';
-	const request = new XMLHttpRequest();
-
-	request.open('POST', url, true);
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send("phone=" + data);
-	console.log(request);
-};
-
-const toggleModal = () => {
-	const MODAL = document.querySelector('.modal');
-	const BACKDROP = document.querySelector(".backdrop");
-	const MODAL_BTN = MODAL.querySelector(".button");
-	const CLOSE_BTN = MODAL.querySelector(".modal__close-btn");
-
-	if (BACKDROP) BACKDROP.addEventListener('click', toggleModal);
-	if (MODAL_BTN) MODAL_BTN.addEventListener('click', toggleModal);
-	if (CLOSE_BTN) CLOSE_BTN.addEventListener('click', toggleModal);
-
-	BACKDROP.classList.toggle('backdrop--open');
-	MODAL.classList.toggle('modal--open');
-	document.body.classList.toggle('fixed');
-}
-
-(function () {
-	const IMAGES = document.querySelectorAll('[data-zoom]');
-	if (IMAGES) {
-		mediumZoom('[data-zoom]', {
-			background: 'rgba(0, 0, 0, 0.8)',
-		});
-	}
-})();
